69bfbfca55ac47c5830f38e208aff370b92dd4fe
aungthuya-jse <aungthuya.jse@outlook.com>
2013-12-23 16:33:37 +0630

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var webRTC = require('webrtc.io').listen(server);
+
+var port = process.env.PORT || 801;
+server.listen(port);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendfile(__dirname + '/style.css');
+});
+
+app.get('/script.js', function(req, res) {
+  res.sendfile(__dirname + '/script.js');
+});
+
+app.get('/webrtc.io.js', function(req, res) {
+  res.sendfile(__dirname + '/webrtc.io.js');
+});
+
