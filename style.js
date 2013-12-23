0617cf663b4442ba60f133b92353bc1bafbe5806
aungthuya-jse <aungthuya.jse@outlook.com>
2013-12-23 16:35:38 +0630

    Create style.js

---
+++ style.js
@@ -0,0 +1,39 @@
+*{
+	padding: 0;
+	margin: 0;
+}
+
+body{
+	padding: 0;
+	width: 640px;
+	margin: 0 auto;
+}
+
+.big{
+	object-fit: contain;
+	top: 260px;
+	left: 400px;
+	background: black;
+	width: 240px;
+	height: 180px;
+	position: relative;
+}
+
+.small{
+	object-fit: contain;
+	top: 380px;
+	left: 560px;
+	background: black;
+	width: 80px;
+	height: 60px;
+	position: relative;
+}
+
+#remoteVideo{
+	object-fit: contain;
+	background: black;
+	height: 480px;
+	width: 640px;
+	position: absolute;
+	z-index: -1;
+}
